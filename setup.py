ad19076a66b0327f83e7328981bcaec502c14f84
Uppu Pavan <uppux.pavan@intel.com>
2022-12-12 13:10:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 import setuptools
 setuptools.setup(
         name="msa_qa_test",
-        version="10.1.1",
-        author="Vinutha",
+        version="12.12.1",
+        author="Pavan",
         Description="This is Guess gamme package",
         packages=["Guesser_game"]
 )
