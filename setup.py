58cf25a95270333a7c8ee4a310fe1c66d6c259ca
Uppu Pavan <uppux.pavan@intel.com>
2022-12-13 09:12:00 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#GuessGame
+import setuptools
+setuptools.setup(
+        name="",
+        version="12.12.1",
+        author="Pavan",
+        Description="This is Guess gamme package",
+        packages=["Guesser_game"]
+)
